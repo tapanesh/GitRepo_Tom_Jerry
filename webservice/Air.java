71a6afc1f94039dea344a05ca21425578a750c0e
Tapanesh Dash <dashtapanesh@gmail.com>
2014-03-11 00:27:39 +0530

    TOM : created getNumOfHotels() in Air

--- webservice/Air.java
+++ webservice/Air.java
@@ -3,3 +3,6 @@ return "Air Details are " + dummyAirDetails ;
 }
 public String getAirAnchorDetails(){
  return "Air Anchor Details " ;
+ public int totalNumOfHotels(){
+ return "total hotels is/ are "+10;
+ }
